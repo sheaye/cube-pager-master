1c2537303d5fd20659f99a12fbc099f27a62a6d0
yexinyan <1134043635@qq.com>
2017-04-11 14:36:40 +0800

    上传sample

--- app/src/main/java/com/sheaye/sample/MainActivity.java
+++ app/src/main/java/com/sheaye/sample/MainActivity.java
@@ -1,13 +1,60 @@
 package com.sheaye.sample;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.sheaye.widget.CubePager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected CubePager mCubePager;
+    private ArrayList<ImageView> mImageViews;
+    private int[] pics = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCubePager = ((CubePager) findViewById(R.id.m_cube_pager));
+        mImageViews = new ArrayList<>();
+        for (int i = 0; i < pics.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.CENTER);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            imageView.setImageResource(pics[i]);
+            mImageViews.add(imageView);
+        }
+        mCubePager.setAutoMove(true);
+        mCubePager.setAdapter(new PicAdapter());
+    }
+
+    class PicAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return mImageViews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return mImageViews.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mImageViews.get(position));
+        }
     }
 }
